f2b4c763884b9969de2390e9ba03266890fe8117
Liam <lw@esyoil.com>
2018-01-12 21:44:38 +0100

    Finish exercise 6.1

--- Exercise6.1/Program.cs
+++ Exercise6.1/Program.cs
@@ -6,7 +6,15 @@ namespace Exercise6._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CPunkt a = new CPunkt(10, 20);
+            CPunkt b = new CPunkt(a);
+            CPunkt c = new CPunkt();
+
+            c.X = 30;
+            c.X = 40;
+
+            Console.WriteLine("Punkt B <" + b.X + "|" + b.Y + ">");
+            Console.WriteLine("Abstand von Punkt B zyu <0|0>: " + b.Abstand());
         }
     }
 }
