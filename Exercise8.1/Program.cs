9d627b0935b75226b93370bbe575962f2e792235
Liam <lw@esyoil.com>
2018-01-15 19:59:03 +0100

    Add CPerson and basic functionality, == and != fail

--- Exercise8.1/Program.cs
+++ Exercise8.1/Program.cs
@@ -6,7 +6,23 @@ namespace Exercise8._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CPerson einePerson = new CPerson("Hans", "Kaiser", "1121", "20.10.1990");
+            CPerson zweitePerson = new CPerson("Karl", "Hansen", "1121", "31.12.1983");
+            
+            if (einePerson == zweitePerson)
+            {
+                Console.WriteLine("Die Personen sind gleich");
+            }
+
+            if (einePerson != zweitePerson)
+            {
+                Console.WriteLine("Die Personen sind nicht gleich");
+            }
+
+            einePerson--;
+            Console.WriteLine(einePerson);
+            zweitePerson++;
+            Console.WriteLine(zweitePerson);
         }
     }
 }
