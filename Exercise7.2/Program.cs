063af5a7fa62aa772449359476e129da4a7f5043
Liam <lw@esyoil.com>
2018-01-15 18:23:52 +0100

    Initial commit 7.2

---
+++ Exercise7.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
