20cb7c40b4f724ee5f043218949357309629a13c
Liam <lw@esyoil.com>
2018-01-15 21:57:13 +0100

    Exercise/8.2 (#4)
    * Initial commit 8.2
    
    * Finish Exercise 8.2

---
+++ Exercise8.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CKonto einKonto = new CKonto();
+            einKonto.NUMMER = "174562322";
+            einKonto.SALDO = 50;
+
+            Console.WriteLine(einKonto);
+
+            Console.WriteLine("250 Euro einzahlen:");
+            einKonto += 250;
+            Console.WriteLine(einKonto);
+            Console.WriteLine();
+
+            Console.WriteLine("30 Euro abheben:");
+            einKonto -= 30;
+
+            double s = (double)einKonto;
+            Console.WriteLine("Das Saldo beträgt: " + s);
+            Console.WriteLine();
+        }
+    }
+}
