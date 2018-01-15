6ad337ddd671b721f7bdd2a7e6e161f5fec5384c
Liam <lw@esyoil.com>
2018-01-15 21:56:41 +0100

    Exercise/2.2 (#6)
    * Initial commit 2.2
    
    * Finish exercise 2.2

---
+++ Exercise2.2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercise2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("###########################################");
+            Console.WriteLine("# C# ist eine moderne Programmiersprache! #");
+            Console.WriteLine("###########################################");
+        }
+    }
+}
