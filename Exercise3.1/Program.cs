fef3e8c6dec3d002d898f3e9bd4cc1943d8869b5
Liam <lw@esyoil.com>
2018-01-16 22:27:13 +0100

    Finish 3.1

--- Exercise3.1/Program.cs
+++ Exercise3.1/Program.cs
@@ -6,7 +6,19 @@ namespace Exercise3._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double startkapital;
+            double zinssatz;
+            double endkapital;
+
+            Console.WriteLine("Gib das Startkapital ein:");
+            startkapital = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Gib den Zinssatz in Prozent ein:");
+            zinssatz = Convert.ToDouble(Console.ReadLine());
+
+            endkapital = startkapital * Math.Pow(1 + zinssatz / 100.0, 3);
+
+            Console.WriteLine("Das Endkapital beträgt: " + endkapital);
         }
     }
 }
