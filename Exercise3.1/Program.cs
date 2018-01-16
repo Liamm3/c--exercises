5b3773d0313c7a2508ac2eab700b890bb2d4691f
Liam <lw@esyoil.com>
2018-01-16 22:28:02 +0100

    Exercise/3.1 (#7)
    * Initial commit 3.1
    
    * Finish 3.1

---
+++ Exercise3.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double startkapital;
+            double zinssatz;
+            double endkapital;
+
+            Console.WriteLine("Gib das Startkapital ein:");
+            startkapital = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Gib den Zinssatz in Prozent ein:");
+            zinssatz = Convert.ToDouble(Console.ReadLine());
+
+            endkapital = startkapital * Math.Pow(1 + zinssatz / 100.0, 3);
+
+            Console.WriteLine("Das Endkapital beträgt: " + endkapital);
+        }
+    }
+}
